332e2ee11f51c20bb48012c47859ee75c5075bbf
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-05-24 16:07:06 +0700

    dah ada penjelasannya

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-camera.position.set( 0, 3, 15 );
+camera.position.set( 0, 50, -20 );
 camera.lookAt( 0, 0, -20 );
 
 const loader = new GLTFLoader();
@@ -42,14 +42,15 @@ dirLight.shadow.camera.near = 0.1;
 dirLight.shadow.camera.far = 40;
 scene.add( dirLight );
 
-
-// Mulai gerakan
+// Titik titik belok
 const curve = new THREE.CatmullRomCurve3([
     new THREE.Vector3(-9, -0.15, 2.1),
     new THREE.Vector3(3, -0.15, -4),
     new THREE.Vector3(13, -0.15, -5.3),
+    new THREE.Vector3(22.5, -0.15, -0.9),
     new THREE.Vector3(33, -0.15, 3.5),
     new THREE.Vector3(39, -0.15, -2.2),
+    new THREE.Vector3(44, -0.15, -7.2),
     new THREE.Vector3(50, -0.15, -12),
     new THREE.Vector3(55, -0.15, -15.5),
     new THREE.Vector3(59, -0.15, -20),
@@ -60,42 +61,39 @@ const curve = new THREE.CatmullRomCurve3([
     new THREE.Vector3(20, -0.15, -44.2),
     new THREE.Vector3(0, -0.15, -45.3),
     new THREE.Vector3(-20, -0.15, -46.3),
+    new THREE.Vector3(-31, -0.15, -46.5),
     new THREE.Vector3(-38, -0.15, -46.6),
     new THREE.Vector3(-43, -0.15, -45),
     new THREE.Vector3(-47, -0.15, -40),
     new THREE.Vector3(-49, -0.15, -27),
     new THREE.Vector3(-48, -0.15, -15),
     new THREE.Vector3(-42, -0.15, -5),
+    new THREE.Vector3(-36, -0.15, 0),
     new THREE.Vector3(-29.5, -0.15, 5),
     new THREE.Vector3(-23, -0.15, 7.3),
     new THREE.Vector3(-19, -0.15, 7),
     new THREE.Vector3(-9, -0.15, 2.1),
 ]);
-  
-const numPoints = 1000; // Number of points to sample along the curve
 
-const targetPositions = curve.getPoints(numPoints);
-const numVectors = targetPositions.length;
-const targetRotations = []; // Array to store Euler rotations
-  
-  // Calculate Euler rotations along the curve
-for (var i = 0; i < numVectors - 1; i++) {
-    var direction = targetPositions[i + 1].clone().sub(targetPositions[i]);
-    var rotation = new THREE.Euler().setFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 0, 1), direction.normalize()));
+// Array isinya x, y, z posisi track
+const targetPositions = curve.getPoints(700);
+
+const targetRotations = [];
+for (let i = 0; i < targetPositions.length - 1; i++) {
+    const direction = targetPositions[i + 1].clone().sub(targetPositions[i]);
+    const rotation = new THREE.Euler().setFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 0, 1), direction.normalize()));
     targetRotations.push(rotation);
 }
 
-const numFrames = 1; // Number of frames or steps in the animation
 let frameCount = 0;
 let targetIndex = 0;
 
 function animate(){
     requestAnimationFrame( animate );
-    // console.log( "Position : " + car.position.x + " , " + car.position.y + " , " + car.position.z);
-    // console.log( "Rotation : " + car.rotation.x + " , " + car.rotation.y + " , " + car.rotation.z);
 
-    const t = frameCount / numFrames;
+    const t = frameCount;
 
+    // Animasi Posisi Object
     car.position.lerp(targetPositions[targetIndex], t);
     camera.position.lerp({
         x: targetPositions[targetIndex].x,
@@ -103,7 +101,7 @@ function animate(){
         z: targetPositions[targetIndex].z
     }, t);
   
-    // Interpolate rotation
+    // Animasi Rotasi Object
     car.rotation.x = THREE.MathUtils.lerp(car.rotation.x, targetRotations[targetIndex].x, t);
     car.rotation.y = THREE.MathUtils.lerp(car.rotation.y, targetRotations[targetIndex].y, t);
     car.rotation.z = THREE.MathUtils.lerp(car.rotation.z, targetRotations[targetIndex].z, t);
@@ -112,7 +110,7 @@ function animate(){
     camera.rotation.z = THREE.MathUtils.lerp(camera.rotation.z, targetRotations[targetIndex].x, t);
     
     frameCount++;
-    if (frameCount > numFrames) {
+    if (frameCount > 1) {
         frameCount = 0;
         targetIndex++;
         if (targetIndex >= targetPositions.length - 1) {
